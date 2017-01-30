7a4d3c7ac6540959e04504397178b66a153ef5d9
Nikolay Falaleev <nikolasent@gmail.com>
2017-01-31 02:05:14 +0300

    Update ui.R
    style: Remove empty lines
    
    Better styling andcomments

--- ui.R
+++ ui.R
@@ -8,41 +8,31 @@
 #
 
 library(shiny)
-
 shinyUI(fluidPage(
   titlePanel("Body Mass Index (BMI) Calculator"),
-  
   sidebarLayout(sidebarPanel(
-    
     helpText("The body mass index (BMI) calculator. Please, input your mass and heihgt."),
-    
     sliderInput(
         inputId = "height",
         label = strong("Your height in cm:"),
         min = 100,
         max = 240,
         value = 185
-    ),
+    ), #User height input slider
     sliderInput(
       inputId = "mass",
       label = strong("Your weight in kg:"),
       min = 20,
       max = 140,
       value = 74
-    ),
+    ), #User mass input slider
     h5("The BMI is an attempt to quantify the amount of tissue mass (muscle, fat, and bone) in an individual, and then categorize that person as underweight, normal weight, overweight, or obese based on that value."),
     h6("The app was created by Nikolay Falaleev as a Course Project for Coursera Developing Data Products MOOC")
     ),
-    
-    
-  
-    
-    
-    
+                
     mainPanel(
-      uiOutput("user_data"),
-      plotOutput("plot", height = "175px", width = "100%"),
-      uiOutput("results")
-     
+      uiOutput("user_data"), #Text with the user data
+      plotOutput("plot", height = "175px", width = "100%"), #Colorful plot of the user BMI
+      uiOutput("results") #Text results
     ))
 ))
